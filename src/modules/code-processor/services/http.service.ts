5320901f64eab31d23650cdfa2d96509621824f3
Mazene <mmazenezerguine@gmail.com>
2024-05-05 19:01:58 +0200

    adding httpService

---
+++ src/modules/code-processor/services/http.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosResponse } from 'axios';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class HttpService {
+	private readonly axiosInstance;
+
+	constructor(private configService: ConfigService) {
+		this.axiosInstance = axios.create({
+			baseURL: this.configService.get(
+				'CODE_RUNNER_SERVICE_API',
+				'http://127.0.0.1:8080/\n',
+			),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+	}
+
+	async get<T>(url: string): Promise<T> {
+		const response = (await this.axiosInstance.get(url)) as AxiosResponse<T>;
+		return response.data;
+	}
+
+	async post<T>(url: string, data?: any): Promise<T> {
+		const response = (await this.axiosInstance.post(url, data)) as AxiosResponse<T>;
+		return response.data;
+	}
+}
