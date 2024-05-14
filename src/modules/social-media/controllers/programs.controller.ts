2260254dcbb11576b436ad490957553d2a046e5a
LoicZHU <zhu.loic@gmail.com>
2024-05-15 00:01:57 +0200

    style: fix 'programs.controller.ts'

--- src/modules/social-media/controllers/programs.controller.ts
+++ src/modules/social-media/controllers/programs.controller.ts
@@ -8,31 +8,31 @@ import { GetProgramDto } from '../dtos/response/GetProgramDto';
 @Controller('/programs')
 @ApiTags('Programs')
 export class ProgramsController {
-  constructor(private readonly programService: ProgramsService) {}
+	constructor(private readonly programService: ProgramsService) {}
 
-  @Post('')
-  @HttpCode(201)
-  @ApiOkResponse({
-    description: 'program created',
-  })
-  @ApiBadRequestResponse({
-    description: 'missing field',
-  })
-  async create(@Body() payload: CreateProgramDto): Promise<void> {
-    await this.programService.saveProgram(payload);
-  }
+	@Post('')
+	@HttpCode(201)
+	@ApiOkResponse({
+		description: 'program created',
+	})
+	@ApiBadRequestResponse({
+		description: 'missing field',
+	})
+	async create(@Body() payload: CreateProgramDto): Promise<void> {
+		await this.programService.saveProgram(payload);
+	}
 
-  @Get('')
-  @HttpCode(200)
-  @ApiOkResponse({
-    description: 'program created',
-  })
-  @ApiBadRequestResponse({
-    description: 'bad params sent',
-  })
-  async findBy(
-    @Param() params: { type: ProgramVisibilityEnum },
-  ): Promise<GetProgramDto[]> {
-    return await this.programService.getProgramByVisibility(params.type);
-  }
+	@Get('')
+	@HttpCode(200)
+	@ApiOkResponse({
+		description: 'program created',
+	})
+	@ApiBadRequestResponse({
+		description: 'bad params sent',
+	})
+	async findBy(
+		@Param() params: { type: ProgramVisibilityEnum },
+	): Promise<GetProgramDto[]> {
+		return await this.programService.getProgramByVisibility(params.type);
+	}
 }
