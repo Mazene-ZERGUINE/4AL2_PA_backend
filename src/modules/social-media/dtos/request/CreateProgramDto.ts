1151f6364f3037d5b2af505d00af675c9c91bf66
Mazene <mmazenezerguine@gmail.com>
2024-05-15 18:34:29 +0200

    adding swagger properties

--- src/modules/social-media/dtos/request/CreateProgramDto.ts
+++ src/modules/social-media/dtos/request/CreateProgramDto.ts
@@ -2,27 +2,48 @@ import { ProgrammingLanguageEnum } from '../../enums/programming-language.enum';
 import { ProgramVisibilityEnum } from '../../enums/program-visibility.enum';
 import { FileTypesEnum } from '../../enums/file-types.enum';
 import { IsEnum, IsNotEmpty, IsString, ArrayNotEmpty, IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProgramDto {
+	@ApiProperty({
+		description: 'program description text max 500 character',
+	})
 	@IsString()
 	description?: string;
 
+	@ApiProperty({
+		description: 'program language',
+		enum: ProgrammingLanguageEnum,
+		examples: ['javascript', 'python'],
+	})
 	@IsNotEmpty()
 	@IsEnum(ProgrammingLanguageEnum, {
 		message: 'This programming language is not supported',
 	})
 	programmingLanguage: ProgrammingLanguageEnum;
 
+	@ApiProperty({
+		description: 'program source code',
+		example: "console.log('hello wolrd')",
+	})
 	@IsString()
 	@IsNotEmpty()
 	sourceCode: string;
 
+	@ApiProperty({
+		description: 'program visibility to others (public | private | only_followers)',
+		enum: ProgramVisibilityEnum,
+	})
 	@IsString()
 	@IsEnum(ProgramVisibilityEnum, {
 		message: 'Not a valid visibility type',
 	})
 	visibility: ProgramVisibilityEnum;
 
+	@ApiProperty({
+		description: 'files types that the program takes as an input',
+		example: "['txt', 'json']",
+	})
 	@IsArray()
 	@ArrayNotEmpty({
 		message: 'Input types must contain at least one file type',
@@ -33,6 +54,10 @@ export class CreateProgramDto {
 	})
 	inputTypes: FileTypesEnum[];
 
+	@ApiProperty({
+		description: 'files types that the program returns as output',
+		example: "['xlsx']",
+	})
 	@IsArray()
 	@ArrayNotEmpty({
 		message: 'Output types must contain at least one file type',
@@ -43,6 +68,10 @@ export class CreateProgramDto {
 	})
 	outputTypes: FileTypesEnum[];
 
+	@ApiProperty({
+		description: 'user id (UUID)',
+		example: '12dx-76fzcw',
+	})
 	@IsString()
 	@IsNotEmpty()
 	userId: string;
