7aae8a0f0fab7aeb6b7eefb5accffda54cb74db2
Mazene <mmazenezerguine@gmail.com>
2024-05-26 22:04:13 +0200

    progrms edit/partail-edit/delete

--- src/modules/social-media/controllers/program.controller.ts
+++ src/modules/social-media/controllers/program.controller.ts
@@ -1,19 +1,29 @@
 import {
 	Body,
 	Controller,
+	Delete,
 	Get,
 	HttpCode,
 	Param,
+	Patch,
 	Post,
+	Put,
 	Query,
 	UseGuards,
 } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+	ApiBadRequestResponse,
+	ApiNoContentResponse,
+	ApiNotFoundResponse,
+	ApiOkResponse,
+	ApiTags,
+} from '@nestjs/swagger';
 import { ProgramsService } from '../services/programs.service';
 import { CreateProgramDto } from '../dtos/request/create-program.dto';
 import { ProgramVisibilityEnum } from '../enums/program-visibility.enum';
 import { GetProgramDto } from '../dtos/response/get-program.dto';
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { ProgramPartialUpdateDto } from '../dtos/request/program-partial-update.dto';
 
 @Controller('/programs')
 @ApiTags('Programs')
@@ -24,8 +34,8 @@ export class ProgramController {
 	@Post()
 	@HttpCode(201)
 	@ApiOkResponse()
+	@ApiBadRequestResponse()
 	async create(@Body() payload: CreateProgramDto): Promise<void> {
-		// throw NotImplementedException();
 		await this.programService.saveProgram(payload);
 	}
 
@@ -35,7 +45,6 @@ export class ProgramController {
 	@ApiOkResponse()
 	@ApiBadRequestResponse()
 	async findBy(@Query('type') type: ProgramVisibilityEnum): Promise<GetProgramDto[]> {
-		// throw NotImplementedException();
 		return await this.programService.getProgramByVisibility(type);
 	}
 	@UseGuards(JwtAuthGuard)
@@ -44,5 +53,40 @@ export class ProgramController {
 	@ApiOkResponse({ description: 'return the list of all user programs' })
 	async getByUser(@Param('userId') userId: string): Promise<GetProgramDto[]> {
 		return this.programService.getUserPrograms(userId);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Put('/:programId')
+	@HttpCode(200)
+	@ApiOkResponse()
+	@ApiBadRequestResponse()
+	@ApiNotFoundResponse()
+	async edit(
+		@Param('programId') programId: string,
+		@Body() payload: CreateProgramDto,
+	): Promise<void> {
+		await this.programService.editProgram(programId, payload);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Patch('/:programId')
+	@HttpCode(200)
+	@ApiOkResponse()
+	@ApiBadRequestResponse()
+	@ApiNotFoundResponse()
+	async updateProgram(
+		@Param('programId') programId: string,
+		@Body() payload: ProgramPartialUpdateDto,
+	): Promise<void> {
+		await this.programService.programPartialUpdate(payload, programId);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Delete('/:programId')
+	@HttpCode(204)
+	@ApiNoContentResponse()
+	@ApiNotFoundResponse()
+	async delete(@Param('programId') programId: string): Promise<void> {
+		await this.programService.deleteProgram(programId);
 	}
 }
