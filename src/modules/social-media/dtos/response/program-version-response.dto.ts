fa7288eb9cf709ffa183253248528743d7edb7c7
Mazene <mmazenezerguine@gmail.com>
2024-05-27 21:26:50 +0200

    doc

--- src/modules/social-media/dtos/response/program-version-response.dto.ts
+++ src/modules/social-media/dtos/response/program-version-response.dto.ts
@@ -1,9 +1,21 @@
 import { ProgrammingLanguageEnum } from '../../enums/programming-language.enum';
 import { ProgramVersionEntity } from '../../entities/program-version.entity';
 import { GetProgramDto } from './get-program.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { ProgramEntity } from '../../entities/program.entity';
 
 export class ProgramVersionResponseDto {
+	@ApiProperty({
+		description: 'original program',
+		type: ProgramEntity,
+	})
 	program: GetProgramDto;
+
+	@ApiProperty({
+		description: 'program versions',
+		isArray: true,
+		type: 'VersionsDto',
+	})
 	versions: VersionsDto[];
 
 	constructor(program: GetProgramDto, versions: VersionsDto[]) {
@@ -13,9 +25,25 @@ export class ProgramVersionResponseDto {
 }
 
 export class VersionsDto {
+	@ApiProperty({
+		description: 'version id',
+		type: 'string',
+	})
 	programVersionId: string;
+	@ApiProperty({
+		description: 'program version text',
+		type: 'number',
+	})
 	version: string;
+	@ApiProperty({
+		description: 'programing language',
+		enum: ProgrammingLanguageEnum,
+	})
 	programmingLanguage: ProgrammingLanguageEnum;
+	@ApiProperty({
+		description: 'soruce code',
+		type: 'string',
+	})
 	sourceCode: string;
 	createdAt: Date;
 	constructor(programVersion: ProgramVersionEntity) {
