943960a7ba707e37e449c44a8ec0a6f58f2b7053
Mazene <mmazenezerguine@gmail.com>
2024-06-09 20:38:57 +0200

    adding loggs for crons

--- src/core/utils/cron.service.ts
+++ src/core/utils/cron.service.ts
@@ -8,31 +8,50 @@ import { join } from 'path';
 export class CronService {
 	@Cron('0 0 */5 * * *') // Runs every 5 hours
 	handleCron() {
-		// Define the directories
 		const inputDir = join(__dirname, '../../../', 'uploads', 'code', 'input');
 		const outputDir = join(__dirname, '../../../', 'uploads', 'code', 'output');
+		const cronFilePath = join(__dirname, '../../../', 'loggs', 'cron', 'cron-log.txt');
 
-		// Function to delete files in a given directory
+		// Ensure the log file exists
+		if (!fs.existsSync(cronFilePath)) {
+			fs.mkdirSync(path.dirname(cronFilePath), { recursive: true });
+			fs.writeFileSync(cronFilePath, '', { flag: 'a' });
+		}
+
+		const logMessage = (message: string) => {
+			const timestamp = new Date().toISOString();
+			fs.appendFile(cronFilePath, `${timestamp} - ${message}\n`, (err) => {
+				if (err) {
+					console.error(`Error writing to log file ${cronFilePath}:`, err);
+				}
+			});
+		};
+
 		const deleteFilesInDir = (dir: string) => {
 			fs.readdir(dir, (err, files) => {
 				if (err) {
-					console.error(`Error reading directory ${dir}:`, err);
+					const errorMsg = `Error reading directory ${dir}: ${err}`;
+					console.error(errorMsg);
+					logMessage(errorMsg);
 					return;
 				}
 				files.forEach((file) => {
 					const filePath = path.join(dir, file);
 					fs.unlink(filePath, (err) => {
 						if (err) {
-							console.error(`Error deleting file ${filePath}:`, err);
+							const errorMsg = `Error deleting file ${filePath}: ${err}`;
+							logMessage(`ERROR: ${errorMsg}`);
 						} else {
-							console.log(`Deleted file ${filePath}`);
+							const successMsg = `Successfully deleted file ${filePath}`;
+							logMessage(`SUCCESS: ${successMsg}`);
 						}
 					});
 				});
 			});
 		};
-
+		logMessage('Cron job started');
 		deleteFilesInDir(inputDir);
 		deleteFilesInDir(outputDir);
+		logMessage('Cron job completed');
 	}
 }
